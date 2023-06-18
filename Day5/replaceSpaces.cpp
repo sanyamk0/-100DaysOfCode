bd737e81e660f8e7d9a4dc797efb8caa78dad09b
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-06-18 14:38:41 +0530

    Day5

---
+++ Day5/replaceSpaces.cpp
@@ -0,0 +1,20 @@
+// Replace Spaces Coding Ninjas
+#include <bits/stdc++.h>
+string replaceSpaces(string &str)
+{
+    string temp;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] == ' ')
+        {
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else
+        {
+            temp.push_back(str[i]);
+        }
+    }
+    return temp;
+}
