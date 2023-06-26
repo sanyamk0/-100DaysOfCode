dedb3ef32dbf29d9205a2911824af496fbda71d1
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-06-26 23:20:32 +0530

    Day13 Optimisation Required

---
+++ Day13/3Sum.cpp
@@ -0,0 +1,28 @@
+// 3Sum-Coding Ninjas
+// Need To Optimise It
+#include <bits/stdc++.h>
+vector<vector<int>>
+findTriplets(vector<int> &arr, int n, int K)
+{
+    set<vector<int>> st;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (arr[i] + arr[j] + arr[k] == K)
+                {
+                    vector<int> temp;
+                    temp.push_back(arr[i]);
+                    temp.push_back(arr[j]);
+                    temp.push_back(arr[k]);
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
