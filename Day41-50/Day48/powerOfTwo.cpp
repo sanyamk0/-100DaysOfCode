528c65e6c4cd504f2303d226c7280164271f73d2
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-07-31 22:55:18 +0530

    Day48

---
+++ Day41-50/Day48/powerOfTwo.cpp
@@ -0,0 +1,17 @@
+// 231. Power of Two - LeetCode
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n == 1)
+        {
+            return true;
+        }
+        else if (n == 0 || n % 2 != 0)
+        {
+            return false;
+        }
+        return isPowerOfTwo(n / 2);
+    }
+};
