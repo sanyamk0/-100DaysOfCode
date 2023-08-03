52e0d3b7c79c1756e0fc8481e7f5bb367c4d616f
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-08-03 22:17:23 +0530

    Day51

---
+++ Day51-60/Day51/maximumSubarray.cpp
@@ -0,0 +1,19 @@
+// 53. Maximum Subarray - LeetCode
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int sum = 0, maximum = nums[0];
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            maximum = max(maximum, sum);
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return maximum;
+    }
+};
