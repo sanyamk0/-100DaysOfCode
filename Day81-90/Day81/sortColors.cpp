8224321a4f3193b3f10ad3f1638beb72a78ad7d4
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-09-02 21:45:33 +0530

    Day81

---
+++ Day81-90/Day81/sortColors.cpp
@@ -0,0 +1,18 @@
+// 75. Sort Colors - LeetCode
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int i = 0, j = 0, k = nums.size() - 1;
+        while (j <= k)
+        {
+            if (nums[j] == 0)
+                swap(nums[i++], nums[j++]);
+            else if (nums[j] == 1)
+                j++;
+            else
+                swap(nums[j], nums[k--]);
+        }
+    }
+};
